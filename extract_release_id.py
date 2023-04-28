73799b0197623aae63d14d5201122489c189670e
Tobeworks <hallo@tobeworks.de>
2023-04-28 09:50:20 +0200

    git changes, fixes

--- extract_release_id.py
+++ extract_release_id.py
@@ -6,8 +6,6 @@ def extract_release_id(url: str) -> str:
     try:
         result = requests.get(url)
         soup = BeautifulSoup(result.content, 'html.parser')
-        #release_id = soup.find('div', {'class': 'trackTitle'}).find('a').get('href').split('/')[-1]
-        #release_id = soup.find('a', {'class': 'album-link'}).get('href').split('-')[2][1:]
         release_id = soup.find('meta', {'name': 'bc-page-properties'}).get('content')
         release_id_json = json.loads(release_id)
         return release_id_json['item_id']
